ff408c5bb5ef43d1cac9916b1c8b1deff644651f
Tiffany Chen <tiffanychen@Tiffanys-MBP-2.wv.cc.cmu.edu>
2018-10-31 13:26:23 -0400

    core data database setup + whale usdz file added

---
+++ LifeLike/Controllers/BooksCollectionViewControllers.swift
@@ -0,0 +1,9 @@
+//
+//  BooksCollectionViewControllers.swift
+//  LifeLike
+//
+//  Created by Tiffany Chen on 10/31/18.
+//  Copyright © 2018 Devin Fan. All rights reserved.
+//
+
+import Foundation
