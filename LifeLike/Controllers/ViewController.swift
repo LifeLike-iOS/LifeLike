d09ad8d952d575bcecc5228a89bb4046cd9eeb7d
Devin Fan <dbfan@andrew.cmu.edu>
2018-10-31 11:58:40 -0400

    Created project

---
+++ LifeLike/Controllers/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  LifeLike
+//
+//  Created by Devin Fan on 10/29/18.
+//  Copyright © 2018 Devin Fan. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController {
+
+    var sceneView: ARSCNView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        sceneView = ARSCNView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height))
+        
+        let referenceImages: Set<ARReferenceImage> = Set<ARReferenceImage>()
+        
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.detectionImages = referenceImages
+        sceneView?.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        view.addSubview(sceneView!)
+    }
+}
+
