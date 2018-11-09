317e2f9271a992c4bc9fb5f4766bccff302ae9c6
Devin Fan <dbfan@andrew.cmu.edu>
2018-11-09 12:33:59 -0500

    Added multiple image detection and whale and manatee

--- LifeLike/Controllers/ARViewController.swift
+++ LifeLike/Controllers/ARViewController.swift
@@ -13,27 +13,22 @@ import Foundation
 
 class ARViewController: UIViewController {
     
+    @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var sceneView: ARSCNView!
+    
     var currentNode: SCNNode?
     var currentAngle = Float(0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.navigationController!.navigationBar.isHidden = true;
+        self.navigationController!.hidesBarsOnTap = true;
+        
         
         // Do any additional setup after loading the view, typically from a nib.
         
-        let whaleImage = ARReferenceImage((UIImage(named: "Whale")?.cgImage)!, orientation: .up, physicalWidth: 0.5)
-        let referenceImages: Set<ARReferenceImage> = [whaleImage]
-        
-        let configuration = ARWorldTrackingConfiguration()
-        configuration.detectionImages = referenceImages
-        configuration.maximumNumberOfTrackedImages = 1
-        configuration.isLightEstimationEnabled = true
-        
         sceneView?.delegate = self
-        sceneView?.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        resetTrackingConfiguration()
         
         let panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panGesture(sender:)))
         sceneView.addGestureRecognizer(panRecognizer)
@@ -41,57 +36,77 @@ class ARViewController: UIViewController {
         view.addSubview(sceneView!)
     }
     
+    override var prefersStatusBarHidden: Bool {
+        return navigationController?.isNavigationBarHidden == true
+    }
+    
+    override var preferredStatusBarUpdateAnimation: UIStatusBarAnimation {
+        return UIStatusBarAnimation.slide
+    }
+    
     @objc func panGesture(sender: UIPanGestureRecognizer) {
+        guard let node = currentNode else { return }
         let translation = sender.translation(in: sender.view!)
         var newAngle = (Float)(translation.x)*(Float.pi)/180.0
         newAngle += currentAngle
         
-        currentNode?.transform = SCNMatrix4MakeRotation(newAngle, 0, 1, 0)
+        node.transform = SCNMatrix4MakeRotation(newAngle, 0, 1, 0)
         
         if(sender.state == UIGestureRecognizer.State.ended) {
             currentAngle = newAngle
         }
     }
     
+    @objc func tapGesture(sender:  UITapGestureRecognizer) {
+        self.navigationController!.navigationBar.isHidden = false;
+    }
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
         sceneView.session.pause()
+    }
+    
+    @IBAction func refreshButtonPressed(_ sender: Any) {
+        resetTrackingConfiguration()
     }
 }
 
 extension ARViewController: ARSCNViewDelegate {
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
-        if let imageAnchor = anchor as? ARImageAnchor {
-//            let referenceImage = imageAnchor.referenceImage
-            let whaleURL = Bundle.main.url(forResource: "small-dolphin", withExtension: "usdz")!
-            if let whaleNode = SCNReferenceNode(url: whaleURL) {
-                whaleNode.load()
-                node.addChildNode(whaleNode)
-                currentNode = whaleNode
+        if let imageAnchor = anchor as? ARImageAnchor, let imageName = imageAnchor.name {
+            let modelURL = Bundle.main.url(forResource: imageName, withExtension: "usdz")!
+            if let modelNode = SCNReferenceNode(url: modelURL) {
+                modelNode.load()
+                node.addChildNode(modelNode)
+                currentNode = modelNode
             }
-            
-//            let plane = SCNPlane(width: referenceImage.physicalSize.width,
-//                                 height: referenceImage.physicalSize.height)
-//
-//            let planeNode = SCNNode(geometry: plane)
-//            planeNode.opacity = 0.25
-//
            
             node.transform = SCNMatrix4MakeTranslation(0, 0.3, 0)
             node.eulerAngles.x = -.pi / 2
-//
-//            let highlightAction = SCNAction.sequence([.wait(duration: 0.25),
-//                                                      .fadeOpacity(to: 0.85, duration: 1.50),
-//                                                      .fadeOpacity(to: 0.15, duration: 1.50),
-//                                                      .fadeOpacity(to: 0.85, duration: 1.50),
-//                                                      .fadeOut(duration: 0.75),
-//                                                      .removeFromParentNode()])
-//            planeNode.runAction(highlightAction)
-//            node.addChildNode(planeNode)
             
+            DispatchQueue.main.async {
+                self.statusLabel.text = "Image detected: \(imageName)"
+            }
         }
         return node
+    }
+}
+
+private extension ARViewController {
+    func resetTrackingConfiguration() {
+        currentNode = nil
+        let images = ["Whale",  "Dolphin", "Manatee"]
+        let referenceImages: Set<ARReferenceImage> = Set(images.map { (imageName) -> ARReferenceImage in
+            let refImage = ARReferenceImage((UIImage(named: imageName)?.cgImage)!, orientation: .up, physicalWidth: 0.5)
+            refImage.name =  imageName
+            return refImage
+        })
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.detectionImages = referenceImages
+        configuration.maximumNumberOfTrackedImages = 1
+        let options: ARSession.RunOptions = [.resetTracking, .removeExistingAnchors]
+        sceneView.session.run(configuration, options: options)
+        statusLabel.text = "Move camera around to detect images"
     }
 }
