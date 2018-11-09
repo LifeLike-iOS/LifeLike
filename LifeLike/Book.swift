993d561b7cff79b47384a2e688bad3f877881398
Tiffany Chen <tiffanychen@Tiffanys-MacBook-Pro-2.local>
2018-11-08 19:26:25 -0500

    Book, Image structs done + tests written to test functionality

---
+++ LifeLike/Book.swift
@@ -0,0 +1,20 @@
+//
+//  Book.swift
+//  LifeLike
+//
+//  Created by Tiffany Chen on 11/8/18.
+//  Copyright © 2018 Devin Fan. All rights reserved.
+//
+
+import Foundation
+
+public struct Book {
+  var title: String
+  var authors: String
+  var ISBN: String
+  var publisher: String
+  var publicationDate: Date
+  var pageCount: Int
+  var chapterCount: Int
+  var images: [Image]
+}
