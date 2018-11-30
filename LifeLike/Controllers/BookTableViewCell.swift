5d53c5ac8a591b1b862592901867dd504d42413d
Devin Fan <dbfan@andrew.cmu.edu>
2018-11-30 02:21:23 -0500

    Omg it's done

---
+++ LifeLike/Controllers/BookTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BookTableViewCell.swift
+//  LifeLike
+//
+//  Created by Devin Fan on 11/29/18.
+//  Copyright © 2018 Devin Fan. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ LifeLike/Controllers/BookViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  BookViewCell.swift
+//  LifeLike
+//
+//  Created by Devin Fan on 11/29/18.
+//  Copyright © 2018 Devin Fan. All rights reserved.
+//
+
+import UIKit
+
+class BookViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
